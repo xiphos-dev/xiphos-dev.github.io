28cbe86fa170619f5627770051f8fdb2a74288bf
nicolaspino <benjamin.camblor@sansano.usm.cl>
2023-01-30 18:10:38 -0300

    Js fix

--- js/scripts.js
+++ js/scripts.js
@@ -18,26 +18,6 @@
     // Remove no-js class
     $('html').removeClass('no-js');
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
-
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
-
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
-
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
-
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
-    });
-
     // Scroll to top
     $('#to-top').click(function() {
         $('html, body').animate({
